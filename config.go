5bb3fde7e3bc5a3bcb1ea81200252ea9704d3cb4
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-12-06 08:47:08 +0100

    Add config function

--- config.go
+++ config.go
@@ -10,3 +10,54 @@
 //********************************************************************************************************************//
 
 package gosw6
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	Body         []byte
+	AccessToken  bool
+}
+
+// Request is to define the request data
+type Request struct {
+	BaseUrl, BearerToken string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r Request) (*http.Response, error) {
+
+	// Set url
+	url := r.BaseUrl + c.Path
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Define header & check if access token is active
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+
+	if !c.AccessToken {
+		request.Header.Set("Authorization", r.BearerToken)
+	}
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
