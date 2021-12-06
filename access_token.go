7e65ca1a080cca325a3dc81b7f6fc71b53b51dac
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-12-06 08:47:18 +0100

    Add function to get the access token

--- access_token.go
+++ access_token.go
@@ -10,3 +10,61 @@
 //********************************************************************************************************************//
 
 package gosw6
+
+import "encoding/json"
+
+// AccessTokenBody is to structure the data
+type AccessTokenBody struct {
+	ClientId  string `json:"client_id"`
+	GrantType string `json:"grant_type"`
+	Scopes    string `json:"scopes"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+}
+
+// AccessTokenReturn is to decode the json return
+type AccessTokenReturn struct {
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+// AccessToken is to get a bearer token from the system
+func AccessToken(body AccessTokenBody, r Request) (AccessTokenReturn, error) {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return AccessTokenReturn{}, err
+	}
+
+	// Set config for request
+	c := Config{
+		Path:        "/api/oauth/token",
+		Method:      "POST",
+		Body:        convert,
+		AccessToken: false,
+	}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return AccessTokenReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode AccessTokenReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return AccessTokenReturn{}, err
+	}
+
+	// Return data
+	return decode, err
+
+}
