7b1f0944d79b1d9ed58ee2ab338d8ca529300275
Jonas Kwiedor <mail@gowizzard.de>
2023-11-27 11:24:05 +0100

    feat: Add new function for custom order transactions search.

--- search.go
+++ search.go
@@ -919,6 +919,67 @@ type CustomOrderSearchReturn struct {
 	} `json:"errors"`
 }
 
+// CustomOrderTransactionsSearchBody is to define the filter for the request
+type CustomOrderTransactionsSearchBody struct {
+	Ids          []string `json:"ids"`
+	Associations struct {
+		Transactions struct {
+			Associations struct {
+				PaymentMethod struct {
+				} `json:"paymentMethod"`
+			} `json:"associations"`
+		} `json:"transactions"`
+	} `json:"associations"`
+	Includes CustomOrderTransactionsSearchBodyIncludes `json:"includes"`
+}
+
+type CustomOrderTransactionsSearchBodyIncludes struct {
+	Order             []string `json:"order"`
+	OrderTransaction  []string `json:"order_transaction"`
+	StateMachineState []string `json:"state_machine_state"`
+	PaymentMethod     []string `json:"payment_method"`
+}
+
+// CustomOrderTransactionsSearchReturn is to decode the json data
+type CustomOrderTransactionsSearchReturn struct {
+	Total int `json:"total"`
+	Data  []struct {
+		Transactions []struct {
+			PaymentMethod struct {
+				UpdatedAt time.Time `json:"updatedAt"`
+				Name      string    `json:"name"`
+				ApiAlias  string    `json:"apiAlias"`
+			} `json:"paymentMethod"`
+			StateMachineState struct {
+				TechnicalName string `json:"technicalName"`
+				ApiAlias      string `json:"apiAlias"`
+			} `json:"stateMachineState"`
+			CustomFields map[string]string `json:"customFields"`
+			ApiAlias     string            `json:"apiAlias"`
+		} `json:"transactions"`
+		CustomFields map[string]string `json:"customFields"`
+		ApiAlias     string            `json:"apiAlias"`
+	} `json:"data"`
+	Aggregations []interface{} `json:"aggregations"`
+	Errors       []struct {
+		Code   string `json:"code"`
+		Status string `json:"status"`
+		Title  string `json:"title"`
+		Detail string `json:"detail"`
+		Meta   struct {
+			Trace []struct {
+				File     string `json:"file"`
+				Line     int    `json:"line"`
+				Function string `json:"function"`
+				Class    string `json:"class"`
+				Type     string `json:"type"`
+			} `json:"trace"`
+			File string `json:"file"`
+			Line int    `json:"line"`
+		} `json:"meta"`
+	} `json:"errors"`
+}
+
 // CustomOrderSearch is to search orders from date x with currency, lineItems, addresses & transactions
 func CustomOrderSearch(page, limit int, date string, r Request) (CustomOrderSearchReturn, error) {
 
@@ -973,6 +1034,70 @@ func CustomOrderSearch(page, limit int, date string, r Request) (CustomOrderSear
 		err = json.NewDecoder(response.Body).Decode(&decode)
 		if err != nil {
 			return CustomOrderSearchReturn{}, err
+		}
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// CustomOrderTransactionsSearch is to search an order for transactions and payment method
+func CustomOrderTransactionsSearch(id string, r Request) (CustomOrderTransactionsSearchReturn, error) {
+
+	// Define body
+	body := CustomOrderTransactionsSearchBody{
+		Ids: []string{id},
+		Includes: CustomOrderTransactionsSearchBodyIncludes{
+			Order: []string{
+				"transactions",
+				"customFields",
+			},
+			OrderTransaction: []string{
+				"paymentMethod",
+				"stateMachineState",
+				"customFields",
+			},
+			StateMachineState: []string{
+				"technicalName",
+			},
+			PaymentMethod: []string{
+				"name",
+				"updatedAt",
+			},
+		},
+	}
+
+	// Convert body data
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return CustomOrderTransactionsSearchReturn{}, err
+	}
+
+	// Set config for request
+	c := Config{
+		Path:   "/api/search/order",
+		Method: "POST",
+		Body:   convert,
+	}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return CustomOrderTransactionsSearchReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode CustomOrderTransactionsSearchReturn
+
+	// Check response header
+	if response.Status != "204 No Content" {
+		err = json.NewDecoder(response.Body).Decode(&decode)
+		if err != nil {
+			return CustomOrderTransactionsSearchReturn{}, err
 		}
 	}
 
